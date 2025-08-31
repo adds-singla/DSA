2a4856d4c9b7a09a5c5698787dd3468ed93c752b
adds-singla <adds9singla@gmail.com>
2025-08-31 15:19:27 +0530

    Update Additional_Question1.cpp

--- Assignment 3/Additional_Question1.cpp
+++ Assignment 3/Additional_Question1.cpp
@@ -1,3 +1,6 @@
+/*Given an array A, find the nearest smaller element for every element A[i] in the array such that the
+element has an index smaller than i.*/
+
 #include<iostream>
 #include<stack>
 #include<algorithm>
