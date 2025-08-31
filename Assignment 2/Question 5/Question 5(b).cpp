5bada9382572f4f720e2f9b76e091109e6f60528
adds-singla <adds9singla@gmail.com>
2025-08-31 15:08:00 +0530

    Update Question 5(b).cpp

--- Assignment 2/Question 5/Question 5(b).cpp
+++ Assignment 2/Question 5/Question 5(b).cpp
@@ -1,3 +1,8 @@
+/*Space required to store any two-dimensional array is number oƒ rows × number oƒ
+columns. Assuming array is used to store elements of the following matrices,
+implement an efficient way that reduces the space requirement.
+(b) Tri-diagonal Matrix.*/
+
 #include <iostream>
 using namespace std;
 
