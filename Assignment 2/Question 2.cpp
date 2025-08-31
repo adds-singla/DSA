326d768fd5abbe294c745c9b2e6cfa1f47cd7843
adds-singla <adds9singla@gmail.com>
2025-08-31 15:05:00 +0530

    Update Question 2.cpp

--- Assignment 2/Question 2.cpp
+++ Assignment 2/Question 2.cpp
@@ -1,3 +1,8 @@
+/*Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping
+the adjacent elements if they are in wrong order. Code the Bubble sort with the
+following elements:
+64 34 25 12 22 11 90*/
+
 #include <iostream>
 using namespace std;
 
