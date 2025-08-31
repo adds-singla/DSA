4fea0e71e9d7f374880fd0f4f8329de82fad4e25
adds-singla <adds9singla@gmail.com>
2025-08-31 15:16:22 +0530

    Update Question1.cpp

--- Assignment 3/Question1.cpp
+++ Assignment 3/Question1.cpp
@@ -1,3 +1,6 @@
+/*Develop a menu driven program demonstrating the following operations on a Stack using array:
+(i) push(), (ii) pop(), (iii) isEmpty(), (iv) isFull(), (v) display(), and (vi) peek().*/
+
 #include<iostream>
 using namespace std;
 
