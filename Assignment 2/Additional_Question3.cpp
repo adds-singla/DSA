92c5cee1b7ef5aa2aad704932b30116726048cbb
adds-singla <adds9singla@gmail.com>
2025-08-31 15:14:23 +0530

    Update Additional_Question3.cpp

--- Assignment 2/Additional_Question3.cpp
+++ Assignment 2/Additional_Question3.cpp
@@ -1,3 +1,8 @@
+/*String Anagrams
+Given two strings str1 and str2, determine if they form an anagram pair.
+Note: Two strings are considered anagrams if one string can be rearranged to form the other
+string.*/
+
 #include<iostream>
 #include<string>
 #include<vector>
