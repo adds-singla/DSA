434dc9948b052174032685908fb33d291e9d3149
adds-singla <adds9singla@gmail.com>
2025-08-31 14:58:06 +0530

    Update Question 1.cpp

--- Assignment 1/Question 1.cpp
+++ Assignment 1/Question 1.cpp
@@ -1,3 +1,13 @@
+/*Developa Menudrivenprogramtodemonstrate the followingoperations of Arrays
+——MENU——-
+1.CREATE
+2.DISPLAY
+3.INSERT
+4.DELETE
+5.LINEAR SEARCH
+6.EXIT*/
+
+
 #include<iostream>
 using namespace std;
 
