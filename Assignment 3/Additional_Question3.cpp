2d7ad35f72723b5b2dcfdabb142a44ed761490de
adds-singla <adds9singla@gmail.com>
2025-08-31 15:20:29 +0530

    Update Additional_Question3.cpp

--- Assignment 3/Additional_Question3.cpp
+++ Assignment 3/Additional_Question3.cpp
@@ -1,4 +1,7 @@
-/*Write a program that checks if an expression has balanced parentheses.*/
+/*Given an array arr[ ] of integers, the task is to find the Next Greater Element for each element of the
+array in order of their appearance in the array. Note: The Next Greater Element for an element x is
+the first greater element on the right side of x in the array. Elements for which no greater element
+exist, consider the next greater element as -1.*/
 
 #include<iostream>
 #include<stack>
