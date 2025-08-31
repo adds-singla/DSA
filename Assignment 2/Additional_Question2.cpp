00699802efa4219ed366c432323182967b0ff52b
adds-singla <adds9singla@gmail.com>
2025-08-31 15:13:56 +0530

    Update Additional_Question2.cpp

--- Assignment 2/Additional_Question2.cpp
+++ Assignment 2/Additional_Question2.cpp
@@ -1,3 +1,8 @@
+/*String Split Challenge
+You are given a string consisting of lowercase English alphabets. Your task is to determine
+if it's possible to split this string into three non-empty parts (substrings) where one of
+these parts is a substring of both remaining parts*/
+
 #include<iostream>
 #include<string>
 #include<vector>
