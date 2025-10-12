9864eaf40ce303486c58e57a9e596c4db08da605
adds-singla <adds9singla@gmail.com>
2025-10-12 18:55:05 +0530

    Update Question2.cpp

--- Assignment 4/Question2.cpp
+++ Assignment 4/Question2.cpp
@@ -1,3 +1,6 @@
+/*Develop a menu driven program demonstrating the following operations on Circular Queues:
+enqueue(), dequeue(), isEmpty(), isFull(), display(), and peek().*/
+
 #include<iostream>
 using namespace std;
 
