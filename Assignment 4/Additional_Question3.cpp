2e70ff58f18a018e4a55d18f79ce06d69267b2cc
adds-singla <adds9singla@gmail.com>
2025-10-12 19:51:45 +0530

    Update Additional_Question3.cpp

--- Assignment 4/Additional_Question3.cpp
+++ Assignment 4/Additional_Question3.cpp
@@ -1,5 +1,9 @@
-/*Write a program interleave the first half of the queue with second half.
-Sample I/P: 4 7 11 20 5 9 Sample O/P: 4 20 7 5 11 9*/
+/*Given a Queue consisting of first n natural numbers (in random order). The task is to check whether
+the given Queue elements can be arranged in increasing order in another Queue using a stack. The
+operation allowed are:
+1. Push and pop elements from the stack
+2. Pop (Or Dequeue) from the given Queue.
+3. Push (Or Enqueue) in the another Queue.*/
 
 #include <iostream>
 #include <queue>
