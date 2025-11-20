0435e0503cffd8ab22e3a944adf6b948bd8b89be
adds-singla <adds9singla@gmail.com>
2025-11-20 23:04:57 +0530

    Add files via upload

---
+++ Assignment 7/Additional_Question1.cpp
@@ -0,0 +1,20 @@
+/* Majority Element (Boyer-Moore)
+   Time: O(n)
+   Space: O(1)
+*/
+#include <iostream>
+using namespace std;
+
+int majority(int a[], int n){
+    int cand=0, count=0;
+    for(int i=0;i<n;i++){
+        if(count==0) cand=a[i];
+        count += (a[i]==cand) ? 1 : -1;
+    }
+    return cand;
+}
+
+int main(){
+    int a[]={2,2,1,1,1,2,2}, n=7;
+    cout<<majority(a,n);
+}
